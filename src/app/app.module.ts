da180ef52aa5f97c7d8c4eb7cefd9fa9b90e4ca5
DIN SHUB <DINSH@rafael.co.il>
2020-11-25 17:50:14 +0200

    something wrong

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GroupListComponent } from './group-list/group-list.component';
+import { TasksListComponent } from './tasks-list/tasks-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GroupListComponent,
+    TasksListComponent
   ],
   imports: [
     BrowserModule
